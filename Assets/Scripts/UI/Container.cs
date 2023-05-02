1b1aff7cb25dac0b9661576b2cd400ec8edaa539
Domi <73461001+Domiurk@users.noreply.github.com>
2023-05-02 16:11:20 +0300

    Container

---
+++ Assets/Scripts/UI/Container.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Items;
+using UnityEngine;
+
+namespace UI
+{
+    public class Container : MonoBehaviour, IName
+    {
+        public string Name => _name;
+        public bool CanDragIn => _canDragIn;
+        public bool CanDragOut => _canDragOut;
+
+        [SerializeField] private string _name;
+        [SerializeField] private List<Slot> _slots;
+        [SerializeField] private bool _canDragIn;
+        [SerializeField] private bool _canDragOut;
+
+        private void Start()
+            => _slots = new List<Slot>(GetComponentsInChildren<Slot>());
+
+        public bool TryAddItem(Item item)
+        {
+            if(!CanDragIn)
+                return false;
+
+            foreach(Slot slot in _slots.Where(slot => slot.IsEmpty)){
+                AddItem(slot, item);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Drop()
+        {
+            if(!CanDragOut)
+                return;
+        
+        }
+    
+        private void AddItem(Slot slot, Item item)
+        {
+            slot.SetItem(item);
+        }
+    }
+}
